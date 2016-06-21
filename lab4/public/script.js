0bf8ac90b5ed347f862a05778d399b0078818b09
LeeZeitz <andrew.l.zeitz@gmail.com>
2016-06-20 23:27:53 -0700

    board with some hover

---
+++ lab4/public/script.js
@@ -0,0 +1,144 @@
+
+/**
+ * Requests a new board state from the server's /data route.
+ * 
+ * @param cb {function} callback to call when the request comes back from the server.
+ */
+function getData(cb){
+    $.get("/data", function(data, textStatus, xhr){
+        console.log("Response for /data: "+textStatus);  
+
+        // handle any errors here....
+
+        // draw the board....
+        cb(data);  
+
+    }); 
+}
+
+/**
+ * Draws the board to the #canvas element on the page. 
+ *
+ * You may find the following links helpful: 
+ *  - https://api.jquery.com/
+ *  - https://api.jquery.com/append/
+ *  - http://www.tutorialspoint.com/jquery/
+ *  - http://www.w3schools.com/jquery/ 
+ *
+ * @param state {object} - an object representing the state of the board.  
+ */ 
+function drawBoard(state){
+
+    var canvas = $("#canvas"); 
+
+    // Change the height and width of the board here...
+    // everything else should adapt to an adjustable
+    // height and width.
+    var W = 600, H = 600; 
+    canvas.css("height", H); 
+    canvas.css("width", W); 
+
+    // The actual SVG element to add to. 
+    // we make a jQuery object out of this, so that 
+    // we can manipulate it via calls to the jQuery API. 
+    var svg = $(makeSVG(W, H));
+
+    var size = state['size'];
+
+    for (var i = 1; i < size; i++){
+        for (var j = 1; j < size; j++){
+
+            svg.append(makeRectangle(500/size * i, 500/size * j, 500/size, 500/size, 'white'))
+        }
+    }
+
+    var board = state['board'];
+
+    for (var i = 1; i < board.length + 1; i++){
+        console.log(board[i - 1]);
+        var array = board[i - 1];
+        for (var j = 1; j < array.length + 1; j++){
+            if (board[i - 1][j - 1] == 1){
+                svg.append(makeCircle(500/size * j, 500/size * i, 500/size/3, 'black'));
+            }
+            else if (board[i - 1][j - 1] == 2){
+                svg.append(makeCircle(500/size*j, 500/size*i, 500/size/3, 'blue'));
+            }
+            else{
+                svg.append(makeCircle(500/size*j, 500/size*i, 500/size/3, 'white'));
+            }
+        }
+    }
+
+    // TODO: Implement board drawing. 
+    
+    //  You will want to append elements to the 
+    //  svg variable using the svg.append(....) 
+    //  method. 
+
+    // append the svg object to the canvas object.
+    canvas.append(svg);
+
+}
+
+
+/**function drawBoard(state){
+
+    var canvas = $("#canvas"); 
+
+    // Change the height and width of the board here...
+    // everything else should adapt to an adjustable
+    // height and width.
+    var W = 600, H = 600; 
+    canvas.css("height", H); 
+    canvas.css("width", W); 
+
+    // The actual SVG element to add to. 
+    // we make a jQuery object out of this, so that 
+    // we can manipulate it via calls to the jQuery API. 
+    var svg = $(makeSVG(W, H));
+
+    var size = state['size'];
+
+    for (var i = 1; i < size; i++){
+        for (var j = 1; j < size; j++){
+
+            svg.append(makeRectangle(500/size * i, 500/size * j, 500/size, 500/size, 'white'))
+        }
+    }
+
+    var board = state['board'];
+
+    for (var i = 1; i < board.length + 1; i++){
+        console.log(board[i - 1]);
+        var array = board[i - 1];
+        for (var j = 1; j < array.length + 1; j++){
+            if (board[i - 1][j - 1] == 1){
+                svg.append(makeCircle(500/size * j, 500/size * i, 500/size/3, 'black'));
+            }
+            else if (board[i - 1][j - 1] == 2){
+                svg.append(makeCircle(500/size*j, 500/size*i, 500/size/3, 'blue'));
+            }
+        }
+    }
+
+    // TODO: Implement board drawing. 
+    
+    //  You will want to append elements to the 
+    //  svg variable using the svg.append(....) 
+    //  method. 
+
+    // append the svg object to the canvas object.
+    canvas.append(svg);
+
+}
+**/
+
+function init(){
+
+    // do page load things here...
+
+    console.log("Initalizing Page...."); 
+    getData(drawBoard); 
+}
+
