8653a667d5067fc58cb9b1aca9fcc60f0f9a0cfe
andysmit <andysmit@uvic.ca>
2016-07-07 13:42:09 -0700

    Update for signin function.

--- GoProject/public/scriptForSignIn.js
+++ GoProject/public/scriptForSignIn.js
@@ -1,25 +1,15 @@
 window.onload = function(){
-	document.getElementById('ProjectButton').onclick = function newProject(){
+	document.getElementById('SignIn').onclick = function Sign(){
 		
-		var selections = document.getElementById('Projects');
-		var name = document.getElementById('newProjectInput').value;
-		selections.options[selections.options.length] = new Option(name, name);
+		var vis = document.getElementById("visible");
+		console.log ("vis");
+		vis.style.visibility = "visible";
 
 	}
-	document.getElementById('start').onclick = function clockStart() { 
-		startDate = new Date();
-		interval = setInterval(update, 1000);
-		update();
+	document.getElementById('VSAI').onclick = function ChangeToAI() { 
+		var change = document.getElementById("name2");
+		change.innerHTML = "AI"
 
-	}
-	document.getElementById('deleteButton').onclick = function(){
-		var checks = document.getElementsByClassName('task-delete-check');
-		for (var i = 0; i < checks.length; i++){
-			if (checks[i].checked){
-				checks[i].parentNode.parentNode.remove();
-				i--;
-			}
-		}
 	}
 	
 }
